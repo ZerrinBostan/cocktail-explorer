840ebf9e137fdf66db7053b4fe636bc554d9b16d
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-08-01 23:16:33 +0300

    feat: Add Card component and integrate it with Cocktails page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;

0f828fb768e3090050e71f120867922e33ca1171
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-08-02 14:31:18 +0300

    feat: implement localStorage for login and logout

--- src/lib/auth/user.js
+++ src/lib/auth/user.js
@@ -7,15 +7,24 @@ const initialState = {
 const userReducers = {
   login: (state) => {
     state.isAuthenticated = true;
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('isAuthenticated', 'true');
+    }
   },
   logout: (state) => {
     state.isAuthenticated = false;
+    if (typeof window !== 'undefined') {
+      localStorage.removeItem('isAuthenticated');
+    }
   },
 };
 
 const userSlice = createSlice({
   name: 'user',
-  initialState,
+  initialState: {
+    ...initialState,
+    isAuthenticated: typeof window !== 'undefined' ? !!localStorage.getItem('isAuthenticated') : false,
+  },
   reducers: userReducers,
 });
 

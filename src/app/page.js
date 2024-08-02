f051d02f4cc70c66917faaf22038d84ff4a482ef
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-08-02 14:29:36 +0300

    cleanup: remove unused component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
-import Cocktails from './cocktails/page';
 import Login from './login/page';
 
 const Home = () => {
   return (
     <div>
       <Login />
-      {/* <Cocktails /> */}
     </div>
   );
 };

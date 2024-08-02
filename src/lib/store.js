3745dffea96e71bd4baa11549d7af2e0b1a76f3d
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-08-02 14:30:41 +0300

    feat: add persistReducer configuration

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,12 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './auth/user';
 import cocktailReducer from './cocktail/cocktailSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
+
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedUserReducer,
     cocktail: cocktailReducer,
   },
 });
 
+export const persistor = persistStore(store);
 export default store;

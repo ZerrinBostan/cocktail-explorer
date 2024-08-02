5ac1a10f1b192319711e5fbcd44ed0ad3e699fe3
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-08-02 13:40:36 +0300

    refactor: Integrate Axios for API requests and refactor cocktail details fetching to use service layer

--- src/app/cocktail-detail/[name]/page.jsx
+++ src/app/cocktail-detail/[name]/page.jsx
@@ -10,6 +10,7 @@ import {
   increaseQuantity,
 } from "@/lib/cocktail/cocktailSlice";
 import Button from "@/ui/Button";
+import { getCocktailDetails } from "@/services/cocktailService";
 
 const CocktailDetail = () => {
   const [cocktail, setCocktail] = useState(null);
@@ -25,9 +26,18 @@ const CocktailDetail = () => {
   const [quantity, setQuantity] = useState(currentItem ? currentItem.quantity : 1);
 
   useEffect(() => {
-    if (cocktailName) {
-      fetchCocktailDetails(cocktailName);
-    }
+    const fetchCocktailDetailsData = async () => {
+      try {
+        if (cocktailName) {
+          const cocktailData = await getCocktailDetails(cocktailName);
+          setCocktail(cocktailData);
+        }
+      } catch (error) {
+        console.error("Error fetching cocktail details:", error);
+      }
+    };
+
+    fetchCocktailDetailsData();
   }, [cocktailName]);
 
   useEffect(() => {
@@ -35,20 +45,6 @@ const CocktailDetail = () => {
       setQuantity(currentItem.quantity);
     }
   }, [currentItem]);
-
-  const fetchCocktailDetails = async (name) => {
-    try {
-      const response = await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
-      );
-      const data = await response.json();
-      if (data.drinks && data.drinks.length > 0) {
-        setCocktail(data.drinks[0]);
-      }
-    } catch (error) {
-      console.error("Error fetching cocktail details:", error);
-    }
-  };
 
   const handleAddToBasket = (cocktail) => {
     if (currentItem) {
@@ -90,64 +86,64 @@ const CocktailDetail = () => {
   }
 
   return (
-      <div className="bg-white shadow-lg rounded-lg max-w-4xl w-full mx-auto flex flex-col md:flex-row">
-        <div className="md:w-1/2 p-10">
-          <Image
-            src={cocktail.strDrinkThumb}
-            alt={cocktail.strDrink}
-            width={500}
-            height={500}
-            className="w-full h-full object-cover rounded-lg"
-          />
-        </div>
-        <div className="md:w-1/2 p-6 flex flex-col justify-between">
-          <div>
-            <div className="flex justify-between items-center mb-4">
-              <div className="flex items-center space-x-4">
-                <button
-                  onClick={handleDecreaseQuantity}
-                  className="text-red-500 hover:text-red-700"
-                >
-                  -
-                </button>
-                <span className="font-semibold">{quantity}</span>
-                <button
-                  onClick={handleIncreaseQuantity}
-                  className="text-green-500 hover:text-green-700"
-                >
-                  +
-                </button>
-              </div>
+    <div className="bg-white shadow-lg rounded-lg max-w-4xl w-full mx-auto flex flex-col md:flex-row">
+      <div className="md:w-1/2 p-10">
+        <Image
+          src={cocktail.strDrinkThumb}
+          alt={cocktail.strDrink}
+          width={500}
+          height={500}
+          className="w-full h-full object-cover rounded-lg"
+        />
+      </div>
+      <div className="md:w-1/2 p-6 flex flex-col justify-between">
+        <div>
+          <div className="flex justify-between items-center mb-4">
+            <div className="flex items-center space-x-4">
               <button
-                className="text-indigo-600 hover:text-indigo-500 font-medium"
-                onClick={handleBackToProducts}
+                onClick={handleDecreaseQuantity}
+                className="text-red-500 hover:text-red-700"
               >
-                &larr; Turn back
+                -
+              </button>
+              <span className="font-semibold">{quantity}</span>
+              <button
+                onClick={handleIncreaseQuantity}
+                className="text-green-500 hover:text-green-700"
+              >
+                +
               </button>
             </div>
-            <h2 className="text-2xl font-bold text-gray-800">
-              {cocktail.strDrink}
-            </h2>
-            <p className="text-gray-600 mt-2">
-              {truncateInstructions(cocktail.strInstructions)}
-            </p>
-            <div className="mt-4">
-              <span className="font-semibold text-gray-700">
-                Glass: {cocktail.strGlass}
-              </span>
-              <span className="ml-4 font-semibold text-gray-700">
-                Category: {cocktail.strCategory}
-              </span>
-            </div>
+            <button
+              className="text-indigo-600 hover:text-indigo-500 font-medium"
+              onClick={handleBackToProducts}
+            >
+              &larr; Turn back
+            </button>
           </div>
-          <Button
-            text="Sepete Ekle"
-            size="medium"
-            className="mt-4"
-            onClick={() => handleAddToBasket(cocktail)}
-          />
+          <h2 className="text-2xl font-bold text-gray-800">
+            {cocktail.strDrink}
+          </h2>
+          <p className="text-gray-600 mt-2">
+            {truncateInstructions(cocktail.strInstructions)}
+          </p>
+          <div className="mt-4">
+            <span className="font-semibold text-gray-700">
+              Glass: {cocktail.strGlass}
+            </span>
+            <span className="ml-4 font-semibold text-gray-700">
+              Category: {cocktail.strCategory}
+            </span>
+          </div>
         </div>
+        <Button
+          text="Sepete Ekle"
+          size="medium"
+          className="mt-4"
+          onClick={() => handleAddToBasket(cocktail)}
+        />
       </div>
+    </div>
   );
 };
 

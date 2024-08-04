d016c6d553cd77cb8eb192039780531df43e4799
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-08-05 01:20:07 +0300

    refactor: remove redux-persist to handle state management on the server

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,22 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import userReducer from './auth/user';
 import cocktailReducer from './cocktail/cocktailSlice';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedUserReducer = persistReducer(persistConfig, userReducer);
 
 const store = configureStore({
   reducer: {
-    user: persistedUserReducer,
     cocktail: cocktailReducer,
   },
 });
 
-export const persistor = persistStore(store);
 export default store;
